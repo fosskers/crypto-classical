6fd8ba0ef8d1931eabbd1de8efc3a35eeba95805
Colin Woodbury <colingw@gmail.com>
2015-02-22 17:10:02 -0800

    (Types) Using `modular-arithmetic` for Keys now
    - Let's us have automatic mod arithmetic by doing:
        (5 :: ℤ/26) + (10 :: ℤ/26)

--- Crypto/Classical/Cipher/Caesar.hs
+++ Crypto/Classical/Cipher/Caesar.hs
@@ -1,27 +1,24 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Crypto.Classical.Cipher.Caesar where
 
 import           Control.Applicative
 import           Crypto.Classical.Types
-import           Crypto.Number.Generate
 import           Crypto.Random
+import           Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as B
 import           Data.Char
+import           Data.Modular
 
 ---
 
--- | An Integer Ring.
-newtype Z26 = Z26 { z26 :: Integer } deriving (Eq,Show)
-
-instance Key Z26 where
-  key g = Z26 . fst . generateMax g $ 26
-
----  
-
-data Caesar a = Caesar a deriving (Eq,Show,Functor)
+data Caesar a = Caesar { caesar :: a } deriving (Eq,Show,Functor)
 
 instance Applicative Caesar where
   pure = Caesar
@@ -31,26 +28,25 @@ instance Monad Caesar where
   return = pure
   Caesar a >>= f = f a
 
-instance Cipher Z26 Caesar where
-  encrypt (Z26 k) = Caesar . B.map f
+instance Cipher (ℤ/26) Caesar where
+  encrypt k = Caesar . B.map f
     where f c | isLower c = f $ toUpper c
               | not $ isLetter c = c
-              | otherwise = toLetter ((toInt c + k') `mod` 26)
+              | otherwise = toLetter . fromIntegral . unMod $ (toInt c + k)
           toLetter l = chr $ ord 'A' + l
-          toInt    c = ord c - ord 'A'
-          k' = fromIntegral k
+          toInt    c = toMod . toInteger $ ord c - ord 'A'
 
-  decrypt (Z26 k) = encrypt (Z26 (-k))
+  decrypt k = encrypt (-k)
 
 ---
 
 foo :: IO SystemRNG
 foo = fmap cprgCreate createEntropyPool
 
-testIO :: IO (Caesar B.ByteString)
+testIO :: IO (Caesar ByteString)
 testIO = do
   k <- fmap key foo
   return $ encrypt k "I QUITE ENJOY THIS" >>= decrypt k
 
-test :: Z26 -> Caesar B.ByteString
+test :: ℤ/26 -> Caesar ByteString
 test k = encrypt k "abcdefghijklmnopqrstuvwxyz" >>= decrypt k
--- Crypto/Classical/Types.hs
+++ Crypto/Classical/Types.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Crypto.Classical.Types where
 
+import Control.Lens
+import Crypto.Number.Generate
 import Crypto.Random (CPRG)
 import Data.ByteString.Lazy (ByteString)
+import Data.List ((\\))
+import Data.Modular
 
 ---
 
@@ -19,3 +27,13 @@ class Cipher k a | a -> k where
 class Key a where
   -- | Randomly generate a Key.
   key :: CPRG g => g -> a
+
+instance Key (ℤ/26) where
+  key g = toMod . fst . generateMax g $ 26
+
+-- | For Affine Ciphers.
+-- `a` must be coprime with `b` (26).
+instance Key (ℤ/26,ℤ/26) where
+  key g = (a,b) & both %~ toMod
+    where a = ([1,3..25] \\ [13]) !! (fromIntegral . fst . generateMax g $ 26)
+          b = fst $ generateMax g 26
