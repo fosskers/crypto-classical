ef0be104ee2502968aad9c294ddddf5b80ea7598
Colin Woodbury <colingw@gmail.com>
2015-03-25 21:30:34 -0700

    New cipher: Vigenere
    - Essentially a Stream Cipher but with a worse key,
      because its length is less than the message.

---
+++ Crypto/Classical/Cipher/Vigenere.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Crypto.Classical.Cipher.Vigenere where
+
+import           Control.Applicative
+import           Control.Lens
+import           Crypto.Classical.Cipher.Stream
+import           Crypto.Classical.Types
+import qualified Data.ByteString.Lazy.Char8 as B
+import           Data.Modular
+
+---
+
+data Vigenère a = Vigenère { _vigenère :: a } deriving (Eq,Show,Functor)
+makeLenses ''Vigenère
+
+instance Applicative Vigenère where
+  pure = Vigenère
+  Vigenère f <*> Vigenère a = Vigenère $ f a
+
+instance Monad Vigenère where
+  return = pure
+  Vigenère a >>= f = f a
+
+-- | A Vigenère Cipher is just a Stream Cipher with a finite key,
+-- shorter than the length of the plaintext.
+-- Weakness here: key length is a factor of the plaintext length.
+instance Cipher [ℤ/26] Vigenère where
+  encrypt k m = Vigenère . view stream . encrypt (vigKey m k) $ m
+  decrypt k m = Vigenère . view stream . decrypt (vigKey m k) $ m
+
+vigKey :: B.ByteString -> [ℤ/26] -> [ℤ/26]
+vigKey m k = concat . repeat . take n $ k
+  where n = floor . logBase 2 . fromIntegral . B.length $ m
